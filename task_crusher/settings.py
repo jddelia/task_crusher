a0b74913119ff255e13270e52ddf2d14f2d788ff
jddelia <jddelia123@gmail.com>
2018-10-27 19:06:49 -0400

    Updates

--- task_crusher/settings.py
+++ task_crusher/settings.py
@@ -124,13 +124,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
-
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
